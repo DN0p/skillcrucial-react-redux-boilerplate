13c05ad9fd3b6dd4b84a9b737a12aa7e8986ff34
DN0p <darya.dunch@gmail.com>
2020-09-15 15:14:00 +0300

    change import

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path')
-import fs from 'fs'
+const fs = require('fs')
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
